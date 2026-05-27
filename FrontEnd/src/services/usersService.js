7a4e11bec09596cdce3efffee1fab601e347f883
stphnVi <stephanie1117@estudiantec.cr>
2026-05-26 20:59:55 -0600

    fix: arreglar el frontend con los últimos cambios

--- FrontEnd/src/services/usersService.js
+++ FrontEnd/src/services/usersService.js
@@ -1,7 +1,12 @@
 const BASE_URL = '/api/users'
 
 export async function getUserSkills(userId) {
-  const res = await fetch(`${BASE_URL}/${userId}/skills`)
+  const token = localStorage.getItem('token')
+  const res = await fetch(`${BASE_URL}/${userId}/skills`, {
+    headers: {
+      'Authorization': `Bearer ${token}`
+    }
+  })
   if (!res.ok) throw new Error('Error al obtener habilidades del usuario')
   return res.json()
 }
