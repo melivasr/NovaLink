7495d09c8f76a10135bd1627fe8cdcf17200f73d
stphnVi <stephanie1117@estudiantec.cr>
2026-05-26 22:54:42 -0600

    add:token limit

--- services/api-gateway/middleware/verifyToken.js
+++ services/api-gateway/middleware/verifyToken.js
@@ -16,7 +16,7 @@ const verifyToken = (req, res, next) => {
     req.user = decoded;
     next();
   } catch (error) {
-    return res.status(403).json({ success: false, message: 'Token inválido o expirado' });
+    return res.status(401).json({ success: false, message: 'Token inválido o expirado' });
   }
 };
 
